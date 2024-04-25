440fbadea4d192fae743fc6d98c2db2edc1d08f7
Andrey Larionov <maclario@yandex.ru>
2024-04-25 19:15:51 +0300

    Изменена реализация InMemoryHistoryManagerTest

--- test/service/InMemoryHistoryManagerTest.java
+++ test/service/InMemoryHistoryManagerTest.java
@@ -1,181 +1,131 @@
 package service;
 
-import model.EpicTask;
-import model.Subtask;
 import model.Task;
 import model.TaskStatus;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class InMemoryHistoryManagerTest {
-    private static TaskManager taskManager;
+    private static HistoryManager historyManager;
+    private static List<Task> history;
     private static Task task1;
-    private static EpicTask epic2;
-    private static Subtask sub3;
-    private static Task task4;
-    private static Task convertedEpic2;
-    private static Task convertedSub3;
-    private static int task1Id;
-    private static int epic2Id;
-    private static int sub3Id;
-    private static int task4Id;
-
-    @BeforeAll
-    public static void historyShouldBeEmptyIfNoTasksAdded() {
-        taskManager = Managers.getDefault();
-        List<Task> history = taskManager.getHistory();
-        assertTrue(history.isEmpty(), "История должна быть пустой.");
-    }
+    private static Task task2;
+    private static Task task3;
 
     @BeforeEach
     public void beforeEach() {
-        taskManager = Managers.getDefault();
-        task1 = new Task("Title t1", "Description t1");
-        taskManager.createTask(task1);
-        task1Id = taskManager.getId();
-        epic2 = new EpicTask("Title e2", "Description e2");
-        taskManager.createEpicTask(epic2);
-        epic2Id = taskManager.getId();
-        sub3 = new Subtask("Title s3", "Description s3", epic2Id);
-        taskManager.createSubtask(sub3);
-        sub3Id = taskManager.getId();
-        task4 = new Task("Title t4", "Description t4");
-        taskManager.createTask(task4);
-        task4Id = taskManager.getId();
-        taskManager.getTask(task4Id);
+        historyManager = new InMemoryHistoryManager();
+        task1 = new Task("Title of task1", "Description of task1");
+        task1.setId(1);
+        task2 = new Task("Title of task 2", "Description of task2");
+        task2.setId(2);
+        task3 = new Task("Title of task3", "Description of task3");
+        task3.setId(3);
 
-        convertedEpic2 = new Task(epic2.getTitle(), epic2.getDescription());
-        convertedEpic2.setId(epic2Id);
-        convertedSub3 = new Task(sub3.getTitle(), sub3.getDescription());
-        convertedSub3.setId(sub3Id);
-
-        taskManager.getTask(task1Id);
-        taskManager.getSubtask(sub3Id);
-        taskManager.getEpicTask(epic2Id);
-        taskManager.getTask(task4Id);
+        historyManager.add(task1);
+        historyManager.add(task2);
+        historyManager.add(task3);
     }
 
     @Test
     public void shouldAddTasksToHistory() {
-        List<Task> history = taskManager.getHistory();
+        history = historyManager.getHistory();
 
-        assertEquals(4, history.size(), "Размер истории не совпадает.");
-        assertEquals(task1, history.get(0), "Задача не добавлена в историю.");
-        assertEquals(convertedSub3, history.get(1), "Подзадача не добавлена в историю.");
-        assertEquals(convertedEpic2, history.get(2), "Эпик не добавлен в историю.");
+        assertEquals(3, history.size(), "Размер истории не совпадает.");
+        assertEquals(task1, history.get(0), "Задача task1 не добавлена в историю.");
+        assertEquals(task2, history.get(1), "Задача task2 не добавлена в историю.");
+        assertEquals(task3, history.get(2), "Задача task3 не добавлена в историю.");
     }
 
     @Test
     public void historyShouldNotContainsDuplicates() {
-        taskManager.getTask(task1Id);
-        taskManager.getEpicTask(epic2Id);
-        taskManager.getSubtask(sub3Id);
-        taskManager.getTask(task4Id);
+        historyManager.add(task1);
+        historyManager.add(task2);
+        historyManager.add(task3);
+        history = historyManager.getHistory();
 
-        List<Task> history = taskManager.getHistory();
-
-        assertEquals(4, history.size(), "Размер истории увеличился.");
+        assertEquals(3, history.size(), "Размер истории увеличился.");
     }
 
     @Test
     public void historyShouldContainOriginalTasksAfterUpdate() {
-        Task updatedTask1 = new Task("New Title task1", "New Description task1");
-        updatedTask1.setId(task1Id);
-        updatedTask1.setStatus(TaskStatus.IN_PROGRESS);
-        taskManager.updateTask(updatedTask1);
+        Task savedTask1 = new Task(task1.getTitle(), task1.getDescription());
+        savedTask1.setId(task1.getId());
+        Task savedTask2 = new Task(task2.getTitle(), task2.getDescription());
+        savedTask2.setId(task2.getId());
+        Task savedTask3 = new Task(task3.getTitle(), task3.getDescription());
+        savedTask3.setId(task3.getId());
 
-        EpicTask updatedEpic2 = new EpicTask("New Title epic2", "New Description epic2");
-        updatedEpic2.setId(epic2Id);
-        updatedEpic2.setStatus(TaskStatus.IN_PROGRESS);
-        taskManager.updateEpicTask(updatedEpic2);
+        task1.setTitle("Обновленный Title of task1");
+        task1.setDescription("Обновленный Description of task1");
+        task1.setStatus(TaskStatus.DONE);
+        task2.setTitle("Обновленный Title of task2");
+        task2.setDescription("Обновленный Description of task2");
+        task2.setStatus(TaskStatus.DONE);
+        task3.setTitle("Обновленный Title of task3");
+        task3.setDescription("Обновленный Description of task3");
+        task3.setStatus(TaskStatus.DONE);
 
-        Subtask updatedSub3 = new Subtask("New Title sub3", "New Description sub3", epic2Id);
-        updatedSub3.setId(sub3Id);
-        updatedSub3.setStatus(TaskStatus.IN_PROGRESS);
-        taskManager.updateSubtask(updatedSub3);
+        history = historyManager.getHistory();
 
-        List<Task> history = taskManager.getHistory();
-
-        assertEquals("Title t1", history.get(0).getTitle(), "Title задачи изменился.");
-        assertEquals("Description t1", history.get(0).getDescription(), "Description задачи изменился.");
-        assertEquals(TaskStatus.NEW, history.get(0).getStatus(), "Status задачи изменился.");
-
-        assertEquals("Title e2", history.get(2).getTitle(), "Title эпика изменился.");
-        assertEquals("Description e2", history.get(2).getDescription(), "Description эпика изменился.");
-        assertEquals(TaskStatus.NEW, history.get(2).getStatus(), "Status эпика изменился.");
-
-        assertEquals("Title s3", history.get(1).getTitle(), "Title подзадачи изменился.");
-        assertEquals("Description s3", history.get(1).getDescription(), "Description подзадачи изменился.");
-        assertEquals(TaskStatus.NEW, history.get(1).getStatus(), "Status подзадачи изменился.");
+        assertEquals(savedTask1, history.get(0), "Атрибуты задачи id: 1, сохраненной в истории, изменились.");
+        assertEquals(savedTask2, history.get(1), "Атрибуты задачи id: 2, сохраненной в истории, изменились.");
+        assertEquals(savedTask3, history.get(2), "Атрибуты задачи id: 3, сохраненной в истории, изменились.");
     }
 
     @Test
     public void shouldRemoveTaskFromHistoryById() {
-        taskManager.deleteTask(task1Id);
-        List<Task> history = taskManager.getHistory();
+        int randomTaskId = new Random().nextInt(1, 4);
+        historyManager.remove(randomTaskId);
+        history = historyManager.getHistory();
 
-        assertEquals(3, history.size(), "Размер истории не уменьшился на единицу.");
-    }
-
-    @Test
-    public void shouldRemoveEpicFromHistoryById() {
-        taskManager.deleteEpicTask(epic2Id);
-        List<Task> history = taskManager.getHistory();
-
-        assertEquals(2, history.size(), "Из истории должны удалиться и эпик, и его подзадача.");
-    }
-
-    @Test
-    public void shouldRemoveSubtaskFromHistoryById() {
-        taskManager.deleteSubtask(sub3Id);
-        List<Task> history = taskManager.getHistory();
-
-        assertEquals(3, history.size(), "Размер истории не уменьшился на единицу.");
+        assertEquals(2, history.size(), "Размер истории не уменьшился на единицу.");
     }
 
     @Test
     public void shouldRemoveTaskFromStartHistory() {
-        taskManager.deleteTask(task1Id);
-        List<Task> history = taskManager.getHistory();
+        historyManager.remove(1);
+        history = historyManager.getHistory();
 
-        assertEquals(3, history.size(), "Размер истории не уменьшился на единицу.");
-        assertEquals(convertedSub3, history.get(0), "Первым элементом должна стать подзадача c id: 3.");
-        assertEquals(convertedEpic2, history.get(1), "Вторым элементом должен стать эпик с id: 2.");
-        assertEquals(task4, history.get(2), "Третьим и последним элементом должна стать задача с id: 4.");
+        assertEquals(2, history.size(), "Размер истории не уменьшился на единицу.");
+        assertEquals(task2, history.get(0), "Первым элементом должна стать задача c id: 2.");
+        assertEquals(task3, history.get(1), "Вторым и последним элементом должна стать задача с id: 3.");
     }
 
     @Test
     public void shouldRemoveTaskFromMidOfHistory() {
-        taskManager.deleteSubtask(sub3Id);
-        List<Task> history = taskManager.getHistory();
+        historyManager.remove(2);
+        history = historyManager.getHistory();
 
-        assertEquals(3, history.size(), "Размер истории не уменьшился на единицу.");
-        assertEquals(convertedEpic2, history.get(1), "Вторым элементом должен стать эпик с id: 2.");
-        assertEquals(task4, history.get(2), "Третьим и последним элементом должна стать задача с id: 4.");
+        assertEquals(2, history.size(), "Размер истории не уменьшился на единицу.");
+        assertEquals(task1, history.get(0), "Первым элементом должна остаться задача с id: 1.");
+        assertEquals(task3, history.get(1), "Вторым и последним элементом истории должна быть задача с id: 3.");
     }
 
     @Test
     public void shouldRemoveTaskFromEndOfHistory() {
-        taskManager.deleteTask(task4Id);
-        List<Task> history = taskManager.getHistory();
+        historyManager.remove(3);
+        history = historyManager.getHistory();
 
-        assertEquals(3, history.size(), "Резмер истории не уменьшился на единицу.");
-        assertEquals(convertedEpic2, history.get(2), "Последним элементом истории должен стать эпик с id: 2");
+        assertEquals(2, history.size(), "Резмер истории не уменьшился на единицу.");
+        assertEquals(task1, history.get(0), "Первым элементом должна остаться задача с id: 1.");
+        assertEquals(task2, history.get(1), "Вторым и последним элементом истории должна быть задача с id: 2.");
     }
 
     @Test
     public void historyShouldBeEmptyAfterRemoveAllTasks() {
-        taskManager.deleteAllTasks();
-        taskManager.deleteAllEpicTasks();
-        List<Task> history = taskManager.getHistory();
+        historyManager.remove(1);
+        historyManager.remove(2);
+        historyManager.remove(3);
+        history = historyManager.getHistory();
 
-        assertTrue(history.isEmpty(), "История должна быть пустой.");
+        assertTrue(history.isEmpty(), "История должна стать пустой.");
     }
 
 }
